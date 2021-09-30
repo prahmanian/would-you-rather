bc741f4f13eb55aac4c0b528caec6c38e80d9060
Pedram Rahmanian <pedram@gutenmade.com>
2021-09-30 16:36:26 -0700

    updated scaffolding

--- would-you-rather/src/App.js
+++ would-you-rather/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import UserImage from './Components/UserImage'
-import {users} from './starter-code/_DATA'
+import UserImage from './components/UserImage'
+import {users} from './starter/_DATA'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
       <UserImage user={users.tylermcginnis} round={"false"}/>
       <UserImage user={users.sarahedo}/>
       <UserImage user={users.johndoe} round={"false"}/>
@@ -28,3 +14,4 @@ function App() {
 }
 
 export default App;
+ 
