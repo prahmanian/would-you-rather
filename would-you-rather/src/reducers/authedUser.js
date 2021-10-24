1d6c496f3b9593c97347fd8cb7f704e8240153a0
Pedram Rahmanian <pedram@gutenmade.com>
2021-10-24 14:12:38 -0700

    added cases for auto signins

--- would-you-rather/src/reducers/authedUser.js
+++ would-you-rather/src/reducers/authedUser.js
@@ -1,4 +1,4 @@
-import { SIGN_IN_USER, SIGN_OUT_USER } from '../actions/authedUser'
+import { SIGN_IN_USER, SIGN_OUT_USER, SIGN_IN_SESSION_USER, AUTO_SIGN_IN_USER } from '../actions/authedUser'
 
 export default function authedUser (state = null, action) {
     switch(action.type) {
@@ -6,6 +6,10 @@ export default function authedUser (state = null, action) {
             return action.userId
         case SIGN_OUT_USER:
             return null
+        case SIGN_IN_SESSION_USER:
+            return action.userId
+        case AUTO_SIGN_IN_USER:
+            return action.userId
         default:
             return state
     }
